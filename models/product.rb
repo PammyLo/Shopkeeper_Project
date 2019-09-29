f3a20368e42e7c051c35bcb37e2cc78bfb299163
PammyLo <pamela@tuminds.com>
2019-09-29 17:15:32 +0100

    added category to products and update method

--- models/product.rb
+++ models/product.rb
@@ -3,7 +3,7 @@ require('../db/sql_runner')
 class Product
 
   attr_reader :id
-  attr_accessor
+  attr_accessor :product_name, :description, :stock, :cost_price, :selling_price, :low_stock_threshold, :delivery_time, :category
 
   def initialize(details)
     @id = details['id'].to_i if details['id']
@@ -15,14 +15,15 @@ class Product
     @selling_price = details['selling_price'].to_i
     @low_stock_threshold = details['low_stock_threshold'].to_i
     @delivery_time = details['delivery_time'].to_i
+    @category = details['category']
   end
 
   def save
     sql = "INSERT INTO products
-    ( supplier_id, product_name, description, stock, cost_price, selling_price, low_stock_threshold, delivery_time )
-    VALUES ( $1, $2, $3, $4, $5, $6, $7, $8 )
+    ( supplier_id, product_name, description, stock, cost_price, selling_price, low_stock_threshold, delivery_time, category )
+    VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9 )
     RETURNING id"
-    values = [ @supplier_id, @product_name, @description, @stock, @cost_price, @selling_price, @low_stock_threshold, @delivery_time ]
+    values = [ @supplier_id, @product_name, @description, @stock, @cost_price, @selling_price, @low_stock_threshold, @delivery_time, @category ]
     results = SqlRunner.run( sql, values )
     @id = results.first()['id'].to_i
   end
@@ -46,8 +47,13 @@ class Product
     return Product.new( results.first )
   end
 
-  
-
+  def update
+    sql = "UPDATE products
+    SET supplier_id = $1, product_name = $2, description = $3, stock = $4, cost_price = $5, selling_price = $6, low_stock_threshold = $7, delivery_time = $8, category = $9
+    WHERE id = $10"
+    values = [ @supplier_id, @product_name, @description, @stock, @cost_price, @selling_price, @low_stock_threshold, @delivery_time, @category, @id ]
+    SqlRunner.run( sql, values )
+  end
 
 
 end
