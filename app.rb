c21403aaa2fc9545e68e81d422b1d55aac838d07
PammyLo <pamela@tuminds.com>
2019-09-29 19:28:48 +0100

    setup Sinatra structure

---
+++ app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/reloader' )
+# require_relative('controllers/suppliers_controller')
+# require_relative('controllers/products_controller')
+also_reload('controllers/*')
+
+get '/' do
+  erb( :index)
+end
